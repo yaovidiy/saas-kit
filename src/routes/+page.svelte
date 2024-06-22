f2bc4f98b6e208a4e71dd02272c5965c0d72bb25
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-06-22 19:26:06 +0300

    added login component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import Modal from '$lib/components/Modal/Modal.svelte';
+	import Login from '$lib/components/Auth/Login.svelte';
 	const { data } = $props();
 </script>
 
 <Modal></Modal>
+
+<Login></Login>
 
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
