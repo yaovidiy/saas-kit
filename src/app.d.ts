ba97630359a1d40bc2954ded012af06daf919c5a
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-06-22 13:58:47 +0300

    added lucia auth hooks

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,10 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+// src/app.d.ts
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			user: import("lucia").User | null;
+			session: import("lucia").Session | null;
+		}
 	}
 }
 
