2039560a3164d91ac3f29da2b5aa2f5030c11e63
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-06-22 14:31:17 +0300

    added github oAuth method

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,22 +3,37 @@ import { Lucia } from "lucia";
 import { dev } from "$app/environment";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { PrismaClient } from "@prisma/client";
+import { GitHub } from "arctic";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
+
+export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
 
 const client = new PrismaClient();
 
 const adapter = new PrismaAdapter(client.session, client.user);
 
 export const lucia = new Lucia(adapter, {
-	sessionCookie: {
-		attributes: {
-			// set to `true` when using HTTPS
-			secure: !dev
-		}
-	}
+  sessionCookie: {
+    attributes: {
+      secure: !dev
+    }
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      githubId: attributes.github_id,
+      username: attributes.username
+    };
+  }
 });
-
 declare module "lucia" {
-	interface Register {
-		Lucia: typeof lucia;
-	}
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  github_id: number;
+  username: string;
 }
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+
+export async function load(event) {
+  return {
+    username: event.locals.user?.username
+  }
+}
