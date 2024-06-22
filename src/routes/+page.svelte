a32fed07bc44e40a991112dc56ce6263392135e1
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-06-22 19:31:35 +0300

    added signup component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import Modal from '$lib/components/Modal/Modal.svelte';
 	import Login from '$lib/components/Auth/Login.svelte';
+	import SignUp from '$lib/components/Auth/SignUp.svelte';
 	const { data } = $props();
 </script>
 
 <Modal></Modal>
 
 <Login></Login>
+<SignUp></SignUp>
 
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
