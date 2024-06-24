cd5188dffea8da39a5e0c250e8875c5d7789a1de
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-06-24 22:16:09 +0300

    added tooltip component

---
+++ src/lib/components/ui/Tooltip/Tooltip.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let {
+		tooltip,
+		isOpen,
+		type,
+		trigger
+	}: {
+		tooltip: string;
+		isOpen?: boolean;
+		type?: 'error' | 'success' | 'warning' | 'info' | 'primary' | 'secondary' | 'accent';
+		trigger?: Snippet;
+	} = $props();
+
+	const tooltipClasses = {
+		error: 'tooltip-error',
+		success: 'tooltip-success',
+		warning: 'tooltip-warning',
+		info: 'tooltip-info',
+		primary: 'tooltip-primary',
+		secondary: 'tooltip-secondary',
+		accent: 'tooltip-accent'
+	};
+</script>
+
+<div
+	class={`tooltip ${type ? tooltipClasses[type] : ''} ${isOpen ? 'tooltip-open' : ''}`}
+	data-tip={tooltip}
+>
+	{#if trigger}
+		{@render trigger()}
+	{:else}
+		<button class="btn btn-neutral">Hover me!</button>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import Table from '$lib/components/ui/Table/Table.svelte';
 	import Alert from '$lib/components/ui/Alert/Alert.svelte';
 	import ToastContainer from '$lib/components/ui/Toasts/ToastContainer.svelte';
+	import Tooltip from '$lib/components/ui/Tooltip/Tooltip.svelte';
 
 	const { data } = $props();
 	const accordionItems = [
@@ -108,3 +109,21 @@
 <Button type="primary" onclick={addDummyToast}>Add toast</Button>
 
 <ToastContainer toasts={dummyToasts}></ToastContainer>
+
+<Tooltip tooltip="Icon tooltip">
+	{#snippet trigger()}
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			class="h-6 w-6 shrink-0 stroke-current cursor-pointer"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+			></path>
+		</svg>
+	{/snippet}
+</Tooltip>
