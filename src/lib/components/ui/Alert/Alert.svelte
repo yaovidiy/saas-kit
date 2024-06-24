7c151567ea3a2086629df18392c08465ee207f8f
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-06-24 10:31:49 +0300

    Added alert component

---
+++ src/lib/components/ui/Alert/Alert.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let {
+		alertText,
+		alertType,
+		content
+	}: {
+		alertText: string;
+		alertType: 'success' | 'error' | 'warning' | 'info';
+		content?: Snippet<[string]>;
+	} = $props();
+	const alertClasses = {
+		success: 'alert-success',
+		error: 'alert-error',
+		warning: 'alert-warning',
+		info: 'alert-info'
+	};
+</script>
+
+<div role="alert" class={`alert ${alertClasses[alertType]}`}>
+	{#if content}
+		{@render content(alertText)}
+	{:else}
+		<span>{alertText}</span>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import Dropdown from '$lib/components/ui/Dropdown/Dropdown.svelte';
 	import Accordion from '$lib/components/ui/Accordion/Accordion.svelte';
 	import Table from '$lib/components/ui/Table/Table.svelte';
+	import Alert from '$lib/components/ui/Alert/Alert.svelte';
 
 	const { data } = $props();
 	const accordionItems = [
@@ -67,3 +68,24 @@
 		</td>
 	{/snippet}
 </Table>
+
+<Alert alertType="error" alertText="Something whent wrong">
+	{#snippet content(alertText)}
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6 shrink-0 stroke-current"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+		<span>{alertText}</span>
+	{/snippet}
+</Alert>
+
+<Alert alertType="info" alertText="Just an info for you"></Alert>
