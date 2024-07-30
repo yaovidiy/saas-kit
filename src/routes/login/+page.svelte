2a882c7587371ab7cefe6ee586f0d95dfe7a79a8
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-07-30 21:58:08 +0300

    login: updated login ui

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,15 +1,19 @@
 <!-- routes/login/+page.svelte -->
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import Input from '$lib/components/ui/Input/Input.svelte';
+	import Button from '$lib/components/ui/Button/Button.svelte';
 </script>
 
-<h1>Sign in</h1>
-<form method="post" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" /><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
-	<button>Continue</button>
-</form>
-<a href="/signup">Sign Up</a>
-<a href="/login/github">Sign in with GitHub</a>
+<div class="card m-auto bg-base-200 shadow-xl md:max-w-96">
+	<div class="card-body">
+		<h1 class="card-title justify-center text-center text-4xl">Sign In</h1>
+		<form class="flex flex-col gap-3 items-center" method="post" use:enhance>
+			<Input label="Username" extraClasses="max-w-xs" value="" placeholder="Username" type="text" />
+			<Input label="Password" type="password" value="" placeholder="Password" />
+			<Button type="primary">Login</Button>
+		</form>
+		<a href="/signup">Sign Up</a>
+		<a href="/login/github">Sign in with GitHub</a>
+	</div>
+</div>
