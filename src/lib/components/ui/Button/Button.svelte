39876568f3e7775366222c8900b8d63ba59f3f99
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-07-30 21:27:07 +0300

    button: updated type setting

--- src/lib/components/ui/Button/Button.svelte
+++ src/lib/components/ui/Button/Button.svelte
@@ -7,12 +7,23 @@
 		onclick
 	}: {
 		children: Snippet;
-		type: string;
+		type: 'primary' | 'secondary' | 'ghost' | 'outline' | 'error' | 'success' | 'warning' | 'info';
 		extraClasses?: string;
 		onclick?: () => void;
 	} = $props();
 
-	let buttonClasses = `btn text-base font-bold btn-${type} min-h-[50px] md:max-w-[250px] w-full ${extraClasses}`;
+	const buttonTypes = {
+		primary: 'btn-primary',
+		secondary: 'btn-secondary',
+		ghost: 'btn-ghost',
+		outline: 'btn-outline',
+		error: 'btn-error',
+		success: 'btn-success',
+		warning: 'btn-warning',
+		info: 'btn-info'
+	};
+
+	let buttonClasses = `btn text-base font-bold ${buttonTypes[type]} min-h-[50px] md:max-w-[250px] w-full ${extraClasses}`;
 </script>
 
 <button {onclick} class={buttonClasses} type="button">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,13 +50,13 @@
 <fieldset class="flex flex-wrap gap-2 mb-10">
 	<legend>Base buttons</legend>
 	<Button type="primary">Primary</Button>
-	<Button extraClasses="btn-secondary" type="">Secondary</Button>
+	<Button type="secondary">Secondary</Button>
 	<Button type="ghost">Ghost</Button>
 	<Button type="outline">Outline</Button>
-	<Button extraClasses="btn-error" type="">Error</Button>
-	<Button extraClasses="btn-success" type="">Success</Button>
-	<Button extraClasses="btn-warning" type="">Warning</Button>
-	<Button extraClasses="btn-info" type="">Info</Button>
+	<Button type="error">Error</Button>
+	<Button type="success">Success</Button>
+	<Button type="warning">Warning</Button>
+	<Button type="info">Info</Button>
 </fieldset>
 
 <fieldset>
